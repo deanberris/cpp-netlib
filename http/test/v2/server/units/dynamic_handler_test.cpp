55ebf867e14928a06e4966c06bb7fa4e493c2ee1
Dean Michael Berris <dberris@google.com>
2014-08-12 22:04:12 +1000

    Change handler definition to match requirements

--- http/test/v2/server/units/dynamic_handler_test.cpp
+++ http/test/v2/server/units/dynamic_handler_test.cpp
@@ -1,3 +1,4 @@
+// Copyright 2014 Google, Inc.
 // Copyright 2014 (c) Dean Michael Berris <dberris@google.com>
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
@@ -11,7 +12,7 @@ namespace http = network::http::v2;
 
 class CustomRequestHandler : public http::request_handler_base {
  public:
-  void register_handler(boost::string_ref path, handler some_handler) override {
+  void register_handler(boost::string_ref, handler) override {
     // Do nothing here.
   };
   ~CustomRequestHandler() {}
