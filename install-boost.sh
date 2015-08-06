c12c5ae956a96189b6215c78fe5fc70ab7f96b1f
Dean Michael Berris <dberris@google.com>
2015-08-07 00:04:32 +1000

    toolset export, create symlinks to updated compilers

--- install-boost.sh
+++ install-boost.sh
@@ -5,7 +5,7 @@ if [ ! -d "${HOME}/${CC}-boost_${BOOST_VERSION}" ]; then
   wget -O boost_${BOOST_VERSION}.tar.bz2 http://sourceforge.net/projects/boost/files/boost/${BOOST_VER}/boost_${BOOST_VERSION}.tar.bz2/download
   tar jxf boost_${BOOST_VERSION}.tar.bz2
   cd boost_${BOOST_VERSION}
-  sh -x ./bootstrap.sh --with-toolset=$CC --prefix=$HOME/$CC-boost_${BOOST_VERSION}
+  sh -x ./bootstrap.sh --with-toolset=$TOOLSET --prefix=$HOME/$CC-boost_${BOOST_VERSION}
   ./b2 --stagedir=. -j2 --build-type=complete --layout=tagged cxxflags='-std=c++11' install
   cd ..
   rm -rf boost_${BOOST_VERSION}
