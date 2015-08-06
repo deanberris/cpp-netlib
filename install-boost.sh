6c43426e0095e7b794c5b1ac68eb4e4112c2b06b
Dean Michael Berris <dberris@google.com>
2015-08-06 22:47:59 +1000

    Try to isolate substitution

--- install-boost.sh
+++ install-boost.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+BOOST_VERSION=${BOOST_VER//./_}
 
-if [ ! -d "$HOME/$CC-boost_${BOOST_VER//./_}" ]; then
-  wget -O boost_${BOOST_VER//./_}.tar.bz2 http://sourceforge.net/projects/boost/files/boost/${BOOST_VER}/boost_${BOOST_VER//./_}.tar.bz2/download
-  tar jxf boost_${BOOST_VER//./_}.tar.bz2
-  cd boost_${BOOST_VER//./_}
-  sh -x ./bootstrap.sh --with-toolset=$CC --prefix=$HOME/$CC-boost_${BOOST_VER//./_}
+if [ ! -d "${$HOME}/${CC}-boost_${BOOST_VERSION}" ]; then
+  wget -O boost_${BOOST_VERSION}.tar.bz2 http://sourceforge.net/projects/boost/files/boost/${BOOST_VER}/boost_${BOOST_VERSION}.tar.bz2/download
+  tar jxf boost_${BOOST_VERSION}.tar.bz2
+  cd boost_${BOOST_VERSION}
+  sh -x ./bootstrap.sh --with-toolset=$CC --prefix=$HOME/$CC-boost_${BOOST_VERSION}
   ./b2 --stagedir=. -j2 --build-type=complete --layout=tagged cxxflags='-std=c++11' install
   cd ..
-  rm -rf boost_${BOOST_VER//./_}
-  rm -rf boost_${BOOST_VER//._}.tar.bz2
+  rm -rf boost_${BOOST_VERSION}
+  rm -rf boost_${BOOST_VERSION}.tar.bz2
 fi
